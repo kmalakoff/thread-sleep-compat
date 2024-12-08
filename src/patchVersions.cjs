f0850ba404dea5ecbafc82fb7fa19d0a1d8c8fcc
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-07 19:01:24 -0800

    Upgrade modules

---
+++ src/patchVersions.cjs
@@ -0,0 +1,16 @@
+const allTargets = require('node-abi').allTargets;
+const semver = require('semver');
+
+function patchVersions() {
+  let abi = null;
+  const runtime = 'node';
+  const target = process.versions.node;
+  for (let i = 0; i < allTargets.length; i++) {
+    const t = allTargets[i];
+    if (t.runtime !== runtime) continue;
+    if (semver.lte(t.target, target)) abi = t.abi;
+    else break;
+  }
+  return abi;
+}
+if (!process.versions.modules) process.versions.modules = patchVersions();
